644657594170087bf42377db37a47b4d68ebdd63
NIASKA <mkman751@yahoo.com>
2021-12-06 15:30:25 -0600

    added initial files

---
+++ client/ios/Pods/Target Support Files/Pods-HusbandoList-HusbandoListTests/Pods-HusbandoList-HusbandoListTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HusbandoList_HusbandoListTests : NSObject
+@end
+@implementation PodsDummy_Pods_HusbandoList_HusbandoListTests
+@end
---
+++ client/ios/Pods/Target Support Files/Pods-HusbandoList/Pods-HusbandoList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HusbandoList : NSObject
+@end
+@implementation PodsDummy_Pods_HusbandoList
+@end
