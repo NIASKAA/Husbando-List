9253789b53bac786344b5854653624045b444723
NIASKA-Desktop <mkman751@yahoo.com>
2021-12-08 12:17:05 -0400

    added very basic footer

---
+++ client/src/component/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {
+    NativeBaseProvider,
+    Box,
+    Text,
+    Heading,
+    VStack,
+    FormControl,
+    Input,
+    Link,
+    Button,
+    Icon,
+    HStack,
+    Center,
+    Pressable,
+  } from 'native-base';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
+const Footer = () => {
+
+    return (
+        <NativeBaseProvider>
+            <Box flex={1} bg="white" safeAreaTop>
+                <Center flex={1}></Center>
+                <HStack bg="white" alignItems="center" safeAreaBottom shadow={6}>
+                    <Pressable
+                        cursor="pointer"
+                        py="3"
+                        flex={1}
+                        onPress={() => setSelected(0)}>
+                        <Center>
+                        <Icon
+                            mb="1"
+                            as={
+                            <MaterialCommunityIcons
+                                name={selected === 0 ? 'home' : 'home-outline'}
+                            />
+                            }
+                            color="white"
+                            size="sm"
+                        />
+                        <Text color="white" fontSize="12">
+                            Search
+                        </Text>
+                        </Center>
+                    </Pressable>
+                    <Pressable
+                        cursor="pointer"
+                        py="2"
+                        flex={1}
+                        onPress={() => setSelected(1)}
+                    >
+                        <Center>
+                        <Icon
+                            mb="1"
+                            as={<MaterialIcons name="search" />}
+                            color="white"
+                            size="sm"
+                        />
+                        <Text color="white" fontSize="12">
+                            Favorites
+                        </Text>
+                        </Center>
+                    </Pressable>
+                </HStack>
+            </Box>
+        </NativeBaseProvider>
+    )
+}
+
+export default Footer
