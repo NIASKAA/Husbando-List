a8e4b7c8d0e67e8ca6441d4a2b972bfb65b586cf
NIASKA-Desktop <mkman751@yahoo.com>
2021-12-14 14:34:47 -0400

    changing model layout for cleaner code

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,4 @@
 const {ProfileModel} = require('./profileModel');
 const {StoriesModel} = require('./storiesModel');
-const {CharactersModel} = require('./charactersModel');
 
-module.exports = {ProfileModel, StoriesModel, CharactersModel};
+module.exports = {ProfileModel, StoriesMode};
