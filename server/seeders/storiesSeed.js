8ab155a46b26803fe9933caa18432f0cebda0d28
NIASKA-Desktop <mkman751@yahoo.com>
2021-12-14 15:34:37 -0400

    added new story

--- server/seeders/storiesSeed.js
+++ server/seeders/storiesSeed.js
@@ -40,6 +40,75 @@ const stories_Seeds = [
         translation: '容疑者たちの甘いたくらみ',
         release_Date: 'May 18, 2016'
     },
+    {   
+        name: 'Liar! Uncover the Truth',
+        characters: [
+            {
+                name:'Haruichi Mamiya',
+                age: 31,
+                image: '',
+                description: 'CEO'
+            },
+            {
+                name:'Sotaro Shiga',
+                age: 29,
+                image: '',
+                description: 'Surgeon'
+            },
+            {
+                name:'Azusa Kurono',
+                age: 27,
+                image: '',
+                description: 'Graphic Designer'
+            },
+            {
+                name:'Keima Katagiri',
+                age: 32,
+                image: '',
+                description: 'Stock Trader'
+            },
+            {
+                name:'Joe Yazawa',
+                age: 32,
+                image: '',
+                description: 'Fashion Designer'
+            },
+            {
+                name:'Itaru Yuikawa',
+                age: 28,
+                image: '',
+                description: 'Foreign Exchange Trader'
+            },
+            {
+                name:'Kazumi Kagami',
+                age: 40,
+                image: '',
+                description: 'Novelist'
+            },
+            {
+                name:'Toya Kashi',
+                age: 28,
+                image: '',
+                description: 'Patisserie'
+            },
+            {
+                name:'Kunio Muroi',
+                age: 38,
+                image: '',
+                description: 'Diplomat'
+            },
+            {
+                name:'Shuto Matsuki',
+                age: 25,
+                image: '',
+                description: 'Soccer Player'
+            },
+        ],
+        image: '',
+        endings: 9,
+        translation: 'ダウト～嘘つきオトコは誰?',
+        release_Date: 'September 1, 2015'
+    },
 ];
 
 const Stories = async () => {
