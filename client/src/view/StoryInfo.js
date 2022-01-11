b4cb8aa7d0d307a5de9797c0dde8d5059e249b9d
NIASKA <mkman751@yahoo.com>
2022-01-11 11:24:36 -0600

    added a map for characters

--- client/src/view/StoryInfo.js
+++ client/src/view/StoryInfo.js
@@ -8,7 +8,27 @@ const StoryInfo = ({route}) => {
     const {name, image, characters, seasons, translation, release_Date} = route.params
     return (
       <>
-
+        <Card>
+            <CardItem header bordered>
+                <Text>{name}</Text>
+                </CardItem>
+                <Image source={{uri: image}} alt={name}/>
+                <CardItem bordered>
+                <Body>
+                    <Text>{seasons}</Text>
+                    <Text>{translation}</Text>
+                    <Text>{release_Date}</Text>
+                    {route.map(() => (
+                        <TouchableOpacity>{characters.name}</TouchableOpacity>
+                    ))}
+                </Body>
+                </CardItem>
+                <CardItem footer bordered>
+                <Text>GeekyAnts</Text>
+            </CardItem>
+        </Card>
+        
+        <BottomNavigation/>
       </>
     )
 }
